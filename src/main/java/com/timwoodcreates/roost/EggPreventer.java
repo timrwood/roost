e9a15e48bd82612a6de8bd54049efd851bf22964
David Vierra <codewarrior@hawaii.rr.com>
2019-02-16 21:15:55 -1000

    Allow egging from non-vanilla chickens

--- src/main/java/com/timwoodcreates/roost/EggPreventer.java
+++ src/main/java/com/timwoodcreates/roost/EggPreventer.java
@@ -7,9 +7,9 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class EggPreventer {
     @SubscribeEvent
     public static void onLivingUpdate(LivingEvent.LivingUpdateEvent e) {
-        if(e.getEntity() instanceof EntityChicken) {
+        if(e.getEntity().getClass().equals(EntityChicken.class)) {
             EntityChicken chicken = (EntityChicken) e.getEntity();
-            if(chicken.timeUntilNextEgg == 0) {
+            if(chicken.timeUntilNextEgg <= 1) {
                 chicken.timeUntilNextEgg = 999999999;
             }
         }
